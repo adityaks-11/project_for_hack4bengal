02653f5e3a553f4420b720152e8e3abf5199ada1
adityaks-11 <adityakrsingh264@gmail.com>
2022-04-09 11:31:52 +0530

    single and double line graph function

--- pymysql.py
+++ pymysql.py
@@ -13,12 +13,21 @@ def to_csv():
         dict1_list=[]
         for k in range(0,len(colum_list)):
             col1=[]
-            for j in range(1,ro+1):
-                ao=colum_list[k]
-                am="enter the"+" "+str(j)+" "+"value of column"+" "+(ao)+":"
-                c1=input(am)
-                col1.append(c1)
-            dict1_list.append(col1)
+            ty=input("is"+" "+colum_list[k]+" "+"an integer? y/n:")
+            if ty=='n':
+                for j in range(1,ro+1):
+                    ao=colum_list[k]
+                    am="enter the"+" "+str(j)+" "+"value of column"+" "+(ao)+":"
+                    c1=input(am)
+                    col1.append(c1)
+                dict1_list.append(col1)
+            elif ty=='y':
+                 for j in range(1,ro+1):
+                    ao=colum_list[k]
+                    am="enter the"+" "+str(j)+" "+"value of column"+" "+(ao)+":"
+                    c1=int(input(am))
+                    col1.append(c1)
+                 dict1_list.append(col1)
         dicto={}
         for m in range (0,len(colum_list)):
             dicto[colum_list[m]]=dict1_list[m]
@@ -27,5 +36,62 @@ def to_csv():
         nm=input("enter the name by which you want your csv file to be saved:")
         df.to_csv(nm+".csv")
         print(nm+".csv file exported successfully") 
+
+def csv_tograph(x):
+    #x=name of the csv file
+    import pandas as pd
+    from matplotlib import pyplot as plt
+    ans='y'
+    while ans=='y':
+        nm=x+'.csv'
+        a= pd.read_csv(nm)
+        print("1.Line graph")
+        print("2.Bar graph")
+        print("3.Histogram")
+        print("4.Pie chart")
+        typ=int(input("enter the number of the type of graph you want:"))
+        if typ==1:
+            pm1=input("enter the name of field from"+" "+nm+" "+"for x-axis:")
+            pm2=input("enter the name of field from"+" "+nm+" "+"for y-axis:")
+            xl=input("enter the label of x-axis:")
+            yl=input("enter the label of y-axis:")
+            titl=input("enter the title of the graph:")
+            plt.plot(a[pm1],a[pm2])
+            plt.xlabel(xl)
+            plt.ylabel(yl)
+            plt.title(titl)
+            plt.show()
+        if typ==2:
+            pm1=input("enter the name of field from"+" "+nm+" "+"for x-axis:")
+            pm2a=input("enter the first line parameter:")
+            pm2b=input("enter the second line parameter:")
+            pm2an=input("enter the name of first parameter:")
+            pm2bn=input("enter the name of second parameter:")
+            xl=input("enter the label of x-axis:")
+            yl=input("enter the label of y-axis:")
+
+            titl=input("enter the title of the graph:")
+            plt.plot(a[pm1],a[pm2a],label=pm2an)
+            plt.plot(a[pm1],a[pm2b],label=pm2bn)
+            plt.xlabel(xl)
+            plt.ylabel(yl)
+            plt.title(titl)
+            plt.legend()
+            plt.show()
+        ans=input("plot a graph again? y/n:")
+
+
+
+
+
+
+
+
+
+
+       
+
 #main
-to_csv()         
+to_csv()
+x=input("enter the name of .csv file")
+csv_tograph(x)         
